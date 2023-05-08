9e2ae9f90c744482705e286c45f1da5b6c57b545
chonlanawawa <chonlanawawa@gmail.com>
2023-05-08 15:48:48 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,24 @@
+# F=int(input("ออกแรงกี่นิวตัน : "))
+# S=int(input("วัตถุเคลื่อนที่กี่เมตร : "))
+# W=F*S
+# print("งานมีค่า",W,"จูล")
+
+# name=input("ใส่ชื่อ:")
+# surname=input("ใส่นามสกุล:")
+# age=int(input("ใส่อายุ:"))
+# age_sister=int(input("ใส่อายุน้อง:"))
+# print("ชื่อ :",name,"\tชนิดของข้อมูล :",type(name))
+# print("นามสกุล :",surname,"\tชนิดของข้อมูล :",type(surname))
+# print("อายุของตนเอง :",age,"\tชนิดของข้อมูล :",type(age))
+# print("อายุที่ห่างกันของตนเองกับน้อง :",age-age_sister)
+
+# x=int(input("x="))
+# y=int(input("y="))
+# result1=x>y
+# print("result=",type(result1),"\t","x>y=",result1)
+
+# print("โปรแกรมคำนวณปริมาตรทรงกรวย\n")
+# r=int(input("มีรัศมีกี่เซนติเมตร : "))
+# h=int(input("มีความสูงกี่เซนติเมตร : "))
+# volume=1/3*3.14*(r**2)*h
+# print("\nปริมาตรของทรงกรวยคือ",volume,"ลูกบาศก์เซนติเมตร")
